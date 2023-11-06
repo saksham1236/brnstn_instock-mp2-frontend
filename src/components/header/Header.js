903b6ce5fc58aa5272cccf062745a36323ed6c34
Kate Bark <barkkate@gmail.com>
2023-11-06 11:53:16 -0500

    MP2-7 and MP2-8 header and footer, font not working, positioning with body el tbd

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,7 +2,6 @@ import "./Header.scss";
 import Logo from "../../assets/Logo/InStock-Logo.svg";
 import { NavLink } from "react-router-dom";
 
-
 function Header() {
   return (
     <header className="header section">
@@ -11,16 +10,10 @@ function Header() {
       </div>
       <div className="header__buttons-container">
         <NavLink to="/" className={({ isActive }) => isActive ? "header__buttons header__button-active" : "header__buttons"} >
-          {/* <NavLink to="/" className={"header__buttons " + {isActive} ? "header__button-active" : ""}> */}
-          {/* <button type="submit" className="header__warehouse-button"> */}
           <p className="header__button-font">Warehouses</p>
-          {/* </button> */}
         </NavLink>
-        {/* <NavLink to="/inventory" className="header__buttons"> */}
         <NavLink to="/inventory" className={({ isActive }) => isActive ? "header__buttons header__button-active" : "header__buttons"}>
-          {/* <button type="submit" className="header__inventory-button"> */}
           <p className="header__button-font">Inventory</p>
-          {/* </button> */}
         </NavLink>
       </div>
     </header>
