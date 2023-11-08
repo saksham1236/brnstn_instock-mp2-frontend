63e6224de9754f62dfadacfc38037ce796bea4b3
Arjun Sahjpaul <a.sahjpaul@gmail.com>
2023-11-07 19:39:08 -0800

    enabled AddWarehouseForm routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import logo from './logo.svg';
 import './styles/App.scss';
-import Header from "./components/Header/Header";
+import Header from "./components/header/Header";
 import Footer from "./components/Footer/Footer";
-
+import AddWarehouseForm from "./components/AddWarehouse/AddWarehouse"
 
 function App() {
   return (
@@ -13,7 +13,7 @@ function App() {
         {/* {<Route path ="/" element = {<Warehouse also known as home page />} />} */}
         {/* {<Route path ="/:warehouse" element = {<Warehouse />} />} */}
         {/* {<Route path ="/:warehouse/edit" element = {<Warehouse />} />} */}
-        {/* {<Route path ="/warehouse/add" element = {<AddWarehouse />} />} */}
+        {<Route path ="/warehouse/add" element = {<AddWarehouseForm />} />}
         {/* {<Route path ="/inventory" element = {<Inventory />} />} */}
         {/* {<Route path ="/inventory/:item" element = {<InventoryItem />} />} */}
         {/* {<Route path ="/inventory/:item/edit" element = {<InventoryItemEdit />} />} */}
