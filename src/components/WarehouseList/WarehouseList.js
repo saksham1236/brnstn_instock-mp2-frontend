0ec97f96428a64f6e7a69f9e43b5773e03f2c908
Michael SDC <michael.sundelacruz@gmail.com>
2023-11-12 09:41:11 -0500

    complete sort block

--- src/components/WarehouseList/WarehouseList.js
+++ src/components/WarehouseList/WarehouseList.js
@@ -1,88 +1,92 @@
 import React from "react";
+import "./WarehouseList.scss";
+import sortImg from "../../assets/Icons/sort-24px.svg";
+import searchImg from "../../assets/Icons/search-24px.svg";
 
 function WarehouseList({ warehousesList }) {
+  const columnHeaderArray = [
+    "Warehouse",
+    "Address",
+    "Contact Name",
+    "Contact Information",
+    "Actions",
+  ];
+
   return (
     <>
       <div className="title-block">
-        <div className="title-block__title">Warehouses</div>
-        <div className="title-block__search">(search fields)</div>
-        <div className="title-block__add">
-          <button className="title-block__add-button">
-            + Add New Warehouse
-          </button>
-        </div>
-      </div>
-
-      <div className="sort-block">
-        <div className="sort-block__column-header--1">
-          <div className="sort-block__header">Warehouse</div>
-          <div className="sort-block__buttons">
-            <div className="sort-block__up">up</div>
-            <div className="sort-block__down">down</div>
+        <div className="title-block__container">
+          <div className="title-block__title">Warehouses</div>
+          <div className="title-block__search">
+            <input
+              type="text"
+              className="title-block__search-bar"
+              placeholder="Search..."
+            />
+            <img className="" src={searchImg} alt="sort icon" />
           </div>
-        </div>
-        <div className="sort-block__column-header--2">
-          <div className="sort-block__header">Address</div>
-          <div className="sort-block__buttons">
-            <div className="sort-block__up">up</div>
-            <div className="sort-block__down">down</div>
-          </div>
-        </div>
-        <div className="sort-block__column-header--3">
-          <div className="sort-block__header">Contact Name</div>
-          <div className="sort-block__buttons">
-            <div className="sort-block__up">up</div>
-            <div className="sort-block__down">down</div>
-          </div>
-        </div>
-        <div className="sort-block__column-header--4">
-          <div className="sort-block__header">Contact Information</div>
-          <div className="sort-block__buttons">
-            <div className="sort-block__up">up</div>
-            <div className="sort-block__down">down</div>
-          </div>
-        </div>
-        <div className="sort-block__column-header--5">
-          <div className="sort-block__header">Actions</div>
-          <div className="sort-block__buttons">
-            <div className="sort-block__up">up</div>
-            <div className="sort-block__down">down</div>
+          <div className="title-block__add">
+            <button className="title-block__add-button">
+              + Add New Warehouse
+            </button>
           </div>
         </div>
       </div>
 
-      <div className="warehouses-block">
-        {warehousesList.map((warehouse) => (
-          <div key={warehouse.id} className="warehouses-block__row">
-            <div className="warehouses-block__warehouse-name">
-              {warehouse.warehouse_name}
-            </div>
-            <div className="warehouses-block__warehouse-address">
-              {warehouse.address}
-            </div>
-            <div className="warehouses-block__warehouse-contact-name">
-              {warehouse.contact_name}
-            </div>
-            <div className="warehouses-block__warehouse-contact-info">
-              <div className="warehouses-block__warehouse-contact-number">
-                {warehouse.contact_phone}
-              </div>
-              <div className="warehouses-block__warehouse-contact-email">
-                {warehouse.contact_email}
-              </div>
-            </div>
-            <div className="warehouses-block__warehouse-actions">
-              <div className="warehouses-block__warehouse-actions-delete">
-                delete
-              </div>
-              <div className="warehouses-block__warehouse-actions-edit">
-                edit
-              </div>
+      <div className="card__background">
+
+        <div className="card__card">
+
+          <div className="sort-block">
+            <div className="sort-block__container">
+              {columnHeaderArray.map((columnHeader, index) => (
+                <div className="sort-block__column-header">
+                  <div className="sort-block__header">{columnHeader}</div>
+                  {index !== 4 && (
+                    <div className="sort-block__buttons">
+                      <div className="sort-block__icons">
+                        <img className="sort-block__icon" src={sortImg} alt="sort icon" />
+                      </div>
+                    </div>
+                  )}
+                </div>
+              ))}
             </div>
           </div>
-        ))}
-      </div>
 
+          <div className="warehouses-block">
+            {warehousesList.map((warehouse) => (
+              <div key={warehouse.id} className="warehouses-block__row">
+                <div className="warehouses-block__warehouse-name">
+                  {warehouse.warehouse_name}
+                </div>
+                <div className="warehouses-block__warehouse-address">
+                  {warehouse.address}
+                </div>
+                <div className="warehouses-block__warehouse-contact-name">
+                  {warehouse.contact_name}
+                </div>
+                <div className="warehouses-block__warehouse-contact-info">
+                  <div className="warehouses-block__warehouse-contact-number">
+                    {warehouse.contact_phone}
+                  </div>
+                  <div className="warehouses-block__warehouse-contact-email">
+                    {warehouse.contact_email}
+                  </div>
+                </div>
+                <div className="warehouses-block__warehouse-actions">
+                  <div className="warehouses-block__warehouse-actions-delete">
+                    delete
+                  </div>
+                  <div className="warehouses-block__warehouse-actions-edit">
+                    edit
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
     </>
   );
 }
