196671fc5e719bc6b417099f20a53f5025a0a1e4
Michael SDC <michael.sundelacruz@gmail.com>
2023-11-12 13:56:23 -0500

    mostly finished -minor edits still required

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,6 +1,5 @@
 import "./Home.scss";
 import { useState, useEffect } from "react";
-import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
 import WarehouseList from "../../components/WarehouseList/WarehouseList";
 
