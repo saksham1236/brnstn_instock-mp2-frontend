eb9d33e81ff941ad05423205f7c25b1e443c4873
Michael SDC <michael.sundelacruz@gmail.com>
2023-11-12 11:51:56 -0500

    completed mobile styling, modified footer

--- src/components/WarehouseList/WarehouseList.js
+++ src/components/WarehouseList/WarehouseList.js
@@ -1,7 +1,11 @@
 import React from "react";
 import "./WarehouseList.scss";
+import { Link } from "react-router-dom";
 import sortImg from "../../assets/Icons/sort-24px.svg";
 import searchImg from "../../assets/Icons/search-24px.svg";
+import deleteImg from "../../assets/Icons/delete_outline-24px.svg";
+import editImg from "../../assets/Icons/edit-24px.svg";
+import chevronImg from "../../assets/Icons/chevron_right-24px.svg";
 
 function WarehouseList({ warehousesList }) {
   const columnHeaderArray = [
@@ -17,15 +21,15 @@ function WarehouseList({ warehousesList }) {
       <div className="title-block">
         <div className="title-block__container">
           <div className="title-block__title">Warehouses</div>
-          <div className="title-block__search">
+          <div className="title-block__search tablet-view">
             <input
               type="text"
               className="title-block__search-bar"
               placeholder="Search..."
             />
-            <img className="" src={searchImg} alt="sort icon" />
+            <img src={searchImg} alt="sort icon" />
           </div>
-          <div className="title-block__add">
+          <div className="title-block__add tablet-view">
             <button className="title-block__add-button">
               + Add New Warehouse
             </button>
@@ -34,8 +38,23 @@ function WarehouseList({ warehousesList }) {
       </div>
 
       <div className="card__background">
-
         <div className="card__card">
+          <div className="search-block mobile-view">
+            <div className="search-block__search">
+              <input
+                type="text"
+                className="search-block__search-bar"
+                placeholder="Search..."
+              />
+              <img src={searchImg} alt="sort icon" />
+            </div>
+
+            <div className="search-block__add">
+              <button className="search-block__add-button">
+                + Add New Warehouse
+              </button>
+            </div>
+          </div>
 
           <div className="sort-block">
             <div className="sort-block__container">
@@ -45,7 +64,11 @@ function WarehouseList({ warehousesList }) {
                   {index !== 4 && (
                     <div className="sort-block__buttons">
                       <div className="sort-block__icons">
-                        <img className="sort-block__icon" src={sortImg} alt="sort icon" />
+                        <img
+                          className="sort-block__icon"
+                          src={sortImg}
+                          alt="sort icon"
+                        />
                       </div>
                     </div>
                   )}
@@ -57,29 +80,54 @@ function WarehouseList({ warehousesList }) {
           <div className="warehouses-block">
             {warehousesList.map((warehouse) => (
               <div key={warehouse.id} className="warehouses-block__row">
-                <div className="warehouses-block__warehouse-name">
-                  {warehouse.warehouse_name}
-                </div>
-                <div className="warehouses-block__warehouse-address">
-                  {warehouse.address}
-                </div>
-                <div className="warehouses-block__warehouse-contact-name">
-                  {warehouse.contact_name}
-                </div>
-                <div className="warehouses-block__warehouse-contact-info">
-                  <div className="warehouses-block__warehouse-contact-number">
-                    {warehouse.contact_phone}
+                <div className="warehouses-block__mobile-block-1">
+                  <div className="warehouses-block__column-1">
+                    <div className="warehouses-block__header mobile-view">
+                      {columnHeaderArray[0]}
+                    </div>
+                    <div className="warehouses-block__warehouse-name">
+                      <Link to={`/warehouses/${warehouse.warehouse_name}`}>
+                        {warehouse.warehouse_name}<img src={chevronImg} alt="chevron icon" />
+                      </Link>
+                    </div>
+                    <div className="warehouses-block__header mobile-view">
+                      {columnHeaderArray[1]}
+                    </div>
+
+                    <div className="warehouses-block__warehouse-address">
+                      {warehouse.address}, {warehouse.city}, {warehouse.country}
+                    </div>
                   </div>
-                  <div className="warehouses-block__warehouse-contact-email">
-                    {warehouse.contact_email}
+                  <div className="warehouses-block__column-2">
+                    <div className="warehouses-block__header mobile-view">
+                      {columnHeaderArray[2]}
+                    </div>
+                    <div className="warehouses-block__warehouse-contact-name">
+                      {warehouse.contact_name}
+                    </div>
+
+                    <div className="warehouses-block__header mobile-view">
+                      {columnHeaderArray[3]}
+                    </div>
+                    <div className="warehouses-block__warehouse-contact-info">
+                      <div className="warehouses-block__warehouse-contact-number">
+                        {warehouse.contact_phone}
+                      </div>
+                      <div className="warehouses-block__warehouse-contact-email">
+                        {warehouse.contact_email}
+                      </div>
+                    </div>
                   </div>
                 </div>
-                <div className="warehouses-block__warehouse-actions">
-                  <div className="warehouses-block__warehouse-actions-delete">
-                    delete
-                  </div>
-                  <div className="warehouses-block__warehouse-actions-edit">
-                    edit
+
+                <div className="warehouses-block__mobile-block-2">
+                  <div className="warehouses-block__warehouse-actions">
+                    <div className="warehouses-block__warehouse-actions-delete">
+                      <img src={deleteImg} alt="delete icon" />
+                    </div>
+                    <div className="warehouses-block__warehouse-actions-edit">
+                      <img src={editImg} alt="edit icon" />
+                    </div>
                   </div>
                 </div>
               </div>
