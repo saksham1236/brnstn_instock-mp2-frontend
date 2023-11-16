3f581dbb462897764320bb68d83401b5f5a740de
Michael SDC <michael.sundelacruz@gmail.com>
2023-11-16 04:11:28 -0500

    starting logic for modal

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -8,26 +8,46 @@ const API_URL = process.env.API_URL || "http://localhost:8080";
 function Home() {
   const [warehousesList, setWarehousesList] = useState([null]);
   const [isLoading, setIsLoading] = useState(true);
+  const [selectedWarehouse, setSelectedWarehouse] = useState(null);
+  const [showModal, setShowModal] = useState(false);
+  const [viewWidth, setViewWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const resize = () => setViewWidth(window.innerWidth);
+    window.addEventListener("resize", resize);
+    return () => window.removeEventListener("resize", resize);
+  }, [viewWidth]);
+
 
   useEffect(() => {
     axios
       .get(API_URL + "/warehouses")
       .then((response) => {
         setWarehousesList(response.data);
-        console.log(response.data);
         setIsLoading(false);
       })
       .catch((error) => {
-        console.log(error);
+        console.error(error);
       });
   }, []);
 
   return (
     <>
-      {!isLoading ? (
-        <WarehouseList warehousesList={warehousesList} />
+
+{/* this code block below needs to be fixed, warehouse list component needs to  */}
+    
+      {((!showModal) && (viewWidth <= 320)) ? (
+        !isLoading ? (
+          <WarehouseList warehousesList={warehousesList} />
+        ) : (
+          <div className="isLoading">Loading...</div>
+        )
       ) : (
-        <div className="isLoading">Loading...</div>
+        !isLoading ? (
+          <WarehouseList warehousesList={warehousesList} />
+        ) : (
+          <div className="isLoading">Loading...</div>
+        )
       )}
     </>
   );
