553e8b08592bce188cd47d4e789c0a2d9ad94072
saksham1236 <saksham1236@gmail.com>
2023-11-16 21:30:16 -0500

    Adding title component for universal use.

--- src/components/TitleComponent/TitleComponent.jsx
+++ src/components/TitleComponent/TitleComponent.jsx
@@ -1,9 +1,8 @@
 import { useNavigate, Link, Navigate } from "react-router-dom";
 import arrowIcon from "../../assets/Icons/arrow_back-24px.svg";
-import './TitleComponent.scss';
-import searchImg from '../../assets/Icons/search-24px.svg'
+import "./TitleComponent.scss";
+import searchImg from "../../assets/Icons/search-24px.svg";
 import ButtonEl from "../Button/Button";
-import editIcon from "../../assets/Icons/edit-24px.svg"
 
 /**
  *
@@ -11,35 +10,60 @@ import editIcon from "../../assets/Icons/edit-24px.svg"
  * @returns Title Component with header
  */
 function TitleComponent(props) {
+	const {
+		title,
+		backButton,
+		buttonType,
+		buttonTitle,
+		buttonLink,
+		searchComponent,
+	} = props;
 
-	const { title, backButton, buttonEl, searchComponent }  = props;
 	const navigate = useNavigate();
 	const backButtonHandler = (event) => {
 		event.preventDefault();
 		navigate(-1);
-	}
-	
+	};
+
 	return (
-		<div className='title-block'>
-			<div className='title-block__container'>
-				
-				<div className='title-block__title'>
-				{backButton && <Link className="title-block__back-button" to = ".." onClick={backButtonHandler}><div className = 'title-block__back-button__img'><img src = {arrowIcon} alt = 'back Icon'/></div></Link>} 
-					{title}
+		<div className='header-block'>
+			<div className={`header-block__container ${buttonType === "edit" ? "header-edit" : ""}`}>
+				<div className='header-block__title'>
+					{backButton && (
+						<Link
+							className='header-block__back-button'
+							to='..'
+							onClick={backButtonHandler}>
+							<div className='header-block__back-button__img'>
+								<img
+									src={arrowIcon}
+									alt='back Icon'
+								/>
+							</div>
+						</Link>
+					)}
+					<h1>{title}</h1>
 				</div>
 
-				{searchComponent && (<div className='title-block__search tablet-view'>
-					<input
-						type='text'
-						className='title-block__search-bar'
-						placeholder='Search...'
-					/>
-					<img
-						src={searchImg}
-						alt='sort icon'
-					/>
-				</div>)}
-				<ButtonEl type = "edit"/>
+				{searchComponent && (
+					<div className='header-block__search tablet-view'>
+						<input
+							type='text'
+							className='header-block__search-bar'
+							placeholder='Search...'
+						/>
+						<img
+							src={searchImg}
+							alt='sort icon'
+						/>
+					</div>
+				)}
+
+				<ButtonEl
+					title={buttonTitle}
+					buttonType={buttonType}
+					link={buttonLink}
+				/>
 			</div>
 		</div>
 	);
