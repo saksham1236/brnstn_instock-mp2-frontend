83f0017338e8a9166ff8ea385d9f6e263a94b5c6
Michael SDC <michael.sundelacruz@gmail.com>
2023-11-18 16:28:57 -0500

    completed delete warehouse modal

--- src/components/DeleteWarehouseModal/DeleteWarehouseModal.jsx
+++ src/components/DeleteWarehouseModal/DeleteWarehouseModal.jsx
@@ -1,18 +1,56 @@
 import React from "react";
 import "./DeleteWarehouseModal.scss";
-import { Link } from "react-router-dom";
-import sortImg from "../../assets/Icons/sort-24px.svg";
-import searchImg from "../../assets/Icons/search-24px.svg";
-import deleteImg from "../../assets/Icons/delete_outline-24px.svg";
-import editImg from "../../assets/Icons/edit-24px.svg";
-import chevronImg from "../../assets/Icons/chevron_right-24px.svg";
+import closeImg from "../../assets/Icons/close-24px.svg";
 
-const DeleteWarehouseModal = () => {
+const DeleteWarehouseModal = ({
+  selectedWarehouse,
+  setShowModal,
+  setSelectedWarehouse,
+}) => {
+  const handleClose = () => {
+    console.log("close modal");
+    setSelectedWarehouse(null);
+    setShowModal(false);
+  };
 
-  
-  return ( <> 
-  Modal Online
-  </> );
-}
- 
+  const handleDelete = () => {
+    console.log("delete selected warehouse: ", selectedWarehouse);
+    setShowModal(false);
+    setSelectedWarehouse(null);
+  };
+
+  return (
+    <div className="delete-warehouse-modal__overlay">
+      <div className="delete-warehouse-modal__container">
+        <div className="delete-warehouse-modal__close">
+          <img src={closeImg} alt="close icon" onClick={handleClose} />
+        </div>
+        <div className="delete-warehouse-modal__title">
+          <h1>Delete {selectedWarehouse} warehouse? </h1>
+        </div>
+        <div className="delete-warehouse-modal__text">
+          <p>
+            Please confirm that you'd like to delete {selectedWarehouse} from
+            the list of warehouses. You won't be able to undo this action.
+          </p>
+        </div>
+        <div className="delete-warehouse-modal__buttons">
+          <button
+            className="delete-warehouse-modal__cancel"
+            onClick={handleClose}
+          >
+            Cancel
+          </button>
+          <button
+            className="delete-warehouse-modal__delete"
+            onClick={handleDelete}
+          >
+            Delete
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default DeleteWarehouseModal;
