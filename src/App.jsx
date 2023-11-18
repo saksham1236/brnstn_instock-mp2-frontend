d0ede1fbb724ad1c5c606ad0bbf4d30379d0ab78
Arjun Sahjpaul <a.sahjpaul@gmail.com>
2023-11-18 15:55:54 -0800

    fixed css leaking issue caused by order of imports

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,12 @@ import './styles/App.scss';
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import Home from "./pages/home/Home";
-import AddWarehouseForm from "./components/AddWarehouse/AddWarehouse";
-import EditWarehouseForm from './components/EditWarehouse/EditWarehouse';
 import InventoryDetails from './components/InventoryDetails/InventoryDetails';
 import EditInventoryForm from "./components/EditInventory/EditInventory";
 import Inventory from "./pages/inventory/Inventory"
+import AddWarehouseForm from "./components/AddWarehouse/AddWarehouse";
+import EditWarehouseForm from './components/EditWarehouse/EditWarehouse';
+
 
 function App() {
   let { itemId } = useParams();
