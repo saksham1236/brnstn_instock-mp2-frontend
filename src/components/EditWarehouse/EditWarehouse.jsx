dc495c23aed6d1a3be356c3892cb007a2e29d075
Arjun Sahjpaul <a.sahjpaul@gmail.com>
2023-11-18 15:11:09 -0800

    imported axios

--- src/components/EditWarehouse/EditWarehouse.jsx
+++ src/components/EditWarehouse/EditWarehouse.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { v4 as uuidv4 } from 'uuid';
+import axios from 'axios';
 
 // pages
 import './EditWarehouse.scss';
