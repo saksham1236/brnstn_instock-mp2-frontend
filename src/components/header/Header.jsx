5f604e416640fe1d99de47e74ca4880d781bb1a5
Arjun Sahjpaul <a.sahjpaul@gmail.com>
2023-11-19 11:31:45 -0800

    added logo link routing to home

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,6 +1,8 @@
 import "./Header.scss";
 import Logo from "../../assets/Logo/InStock-Logo.svg";
 import { NavLink } from "react-router-dom";
+import { Link } from 'react-router-dom';
+
 /**
  * Has class name - header section header__logo header__buttons-container header__button-font
  * @returns Header component for Instock
@@ -9,9 +11,11 @@ function Header() {
   return (
     <header className="header section">
       {/* <div className="header__container"> */}
+      <Link to='/'>
         <div className="header__logo">
           <img src={Logo} alt="logo" className="header__logo" />
         </div>
+        </Link>
         <div className="header__buttons-container">
           <NavLink
             to="/"
