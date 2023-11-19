2c1a17c198392d3afb2219dda5efa5435273dbc7
saksham1236 <saksham1236@gmail.com>
2023-11-19 04:49:07 -0500

    Adding form handling to inventory forms.

--- src/components/AddInventory/AddInventory.jsx
+++ src/components/AddInventory/AddInventory.jsx
@@ -7,118 +7,140 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import { useParams, useNavigate } from "react-router-dom";
 /**
- * 
- * @param {*} props 
+ *
+ * @param {*} props
  * @returns Renders Add Inventory Form component
  */
 
 function AddInventory(props) {
-    
 	const params = useParams();
 	const [itemData, setItemData] = useState();
 	const navigate = useNavigate();
-	// const [formData, setFormData] = useState();
+	const [formData, setFormData] = useState({
+		id: "",
+		warehouse_id: "",
+		item_name: "Television",
+		description: "",
+		category: "",
+		status: "",
+		quantity: "",
+		created_at: "",
+		updated_at: "",
+		warehouse_name: "",
+	});
 
-	const addFormHandler = (event) => {
+	const formHandler = (event) => {
+		axios.post()
 		event.preventDefault();
-		alert(event);
-		navigate(-1);
-	}
-
-
+		console.log(formData);
+	};
+	const onChangeFormhandler = (event) => {
+		setFormData({ ...formData, [event.target.name]: event.target.value });
+	};
 	const backButtonHandler = (event) => {
 		event.preventDefault();
 		navigate(-1);
 	};
-		return (
-			<>
-				<TitleComponent
-					title='Add Inventory Item'
-					backButton={true}
-				/>
+	return (
+		<>
+			<TitleComponent
+				title='Add Inventory Item'
+				backButton={true}
+			/>
 
-				<section className='card-container__background'>
-					<div className='card-container__card'>
-						<div className='inventory-edit__container'>
-							<form className='inventory-edit__form' onSubmit={addFormHandler}>
-								<div className='inventory-edit__form__row'>
-									<div className='inventory-edit__form__column'>
-										<h2 className='inventory-edit__form__header'>
-											Item Details
-										</h2>
-										<InputComponent
-											labelName='Item Name'
-											defaultValue="Add Item Name"
-                                            required
-										/>
-										<InputComponent
-											labelName='Description'
-											type='textarea'
-											defaultValue="Add a description"
-                                            required
-										/>
-										<DropdownSelect
-											labelName='Category'
-											items={[
-                                                "Please select a category",
-												"Electronics",
-												"Gear",
-												"Apparel",
-												"Accessories",
-												"Health",
-											]}
-											defaultValue = "Please select a category"
-                                            required
-										/>
-									</div>
+			<section className='card-container__background'>
+				<div className='card-container__card'>
+					<div className='inventory-edit__container'>
+						<form
+							className='inventory-edit__form'
+							onSubmit={formHandler}
+							onChange={onChangeFormhandler}>
+							<div className='inventory-edit__form__row'>
+								<div className='inventory-edit__form__column'>
+									<h2 className='inventory-edit__form__header'>
+										Item Details
+									</h2>
+									<InputComponent
+										labelName='Item Name'
+										fieldName='item_name'
+										defaultValue='Add Item Name'
+										required
+									/>
+									<InputComponent
+										labelName='Description'
+										type='textarea'
+										defaultValue='Add a description'
+										fieldName='description'
+										required
+									/>
+									<DropdownSelect
+										labelName='Category'
+										items={[
+											"Please select a category",
+											"Electronics",
+											"Gear",
+											"Apparel",
+											"Accessories",
+											"Health",
+										]}
+										defaultValue='Please select a category'
+										fieldName='category'
+										required
+									/>
+								</div>
 
-									<div className='inventory-edit__form__column'>
-										<h2 className='inventory-edit__form__header'>
-											Item Availabilty
-										</h2>
-										<RadioButtons
-											labelName='Status'
-											items={["In Stock", "Out of Stock"]}
-										/>
-                                        <InputComponent
-											labelName='Quantity'
-											defaultValue="0"
-                                            required
-										/>
-										<DropdownSelect
-											labelName='Warehouse'
-											items={[
-                                                "Please select a Warehouse",
-												"Manhattan",
-												"Washington",
-												"Jersey",
-												"San Francisco",
-												"Santa Monica",
-												"Seattle",
-												"Miami",
-												"Boston",
-											]}
-											defaultValue = "Please select a Warehouse"
-										/>
-									</div>
+								<div className='inventory-edit__form__column'>
+									<h2 className='inventory-edit__form__header'>
+										Item Availabilty
+									</h2>
+									<RadioButtons
+										labelName='Status'
+										items={["In Stock", "Out of Stock"]}
+										fieldName='status'
+									/>
+									<InputComponent
+										labelName='Quantity'
+										defaultValue='0'
+										fieldName='quantity'
+										required
+									/>
+									<DropdownSelect
+										labelName='Warehouse'
+										items={[
+											"Please select a Warehouse",
+											"Manhattan",
+											"Washington",
+											"Jersey",
+											"San Francisco",
+											"Santa Monica",
+											"Seattle",
+											"Miami",
+											"Boston",
+										]}
+										defaultValue='Please select a Warehouse'
+										fieldName='warehouse_name'
+									/>
 								</div>
-								<div className='inventory-edit__form__row'>
-									<div className='inventory-edit__form__buttons'>
-										<ButtonEl
-											title='Cancel'
-											buttonType='cancel'
-											onClick = {backButtonHandler}
-										/>
-										<ButtonEl title='Submit' type = "submit"/>
-									</div>
+							</div>
+							<div className='inventory-edit__form__row'>
+								<div className='inventory-edit__form__buttons'>
+									<ButtonEl
+										title='Cancel'
+										buttonType='cancel'
+										onClick={backButtonHandler}
+									/>
+									<ButtonEl
+										title='+ Add Item'
+										type='submit'
+									/>
 								</div>
-							</form>
-						</div>
+							</div>
+						</form>
 					</div>
-				</section>
-			</>
-		);
-
+				</div>
+			</section>
+		</>
+	);
 }
 
 export default AddInventory;
