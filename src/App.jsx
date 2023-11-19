897902a7279a5a52442241c6c28a1a5f39c54e97
Arjun Sahjpaul <a.sahjpaul@gmail.com>
2023-11-19 11:34:01 -0800

    fixed missing brackets at inventory/itemId, fixed AddWarehouseForm  and EditWarehouseForm route paths

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,13 @@ import './styles/App.scss';
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import Home from "./pages/home/Home";
-import AddWarehouseForm from "./components/AddWarehouse/AddWarehouse";
-import EditWarehouseForm from './components/EditWarehouse/EditWarehouse';
 import InventoryDetails from './components/InventoryDetails/InventoryDetails';
 import Inventory from "./pages/inventory/Inventory";
 import WarehouseDetails from "./components/WarehouseDetails/WarehouseDetails";
 import EditInventoryForm from "./components/EditInventory/EditInventory";
+import AddWarehouseForm from "./components/AddWarehouse/AddWarehouse";
+import EditWarehouseForm from './components/EditWarehouse/EditWarehouse';
+
 
 function App() {
   let { itemId } = useParams();
@@ -20,9 +21,9 @@ function App() {
         {/* {<Route path ="/:warehouse" element = {<Warehouse />} />} */}
         {/* {<Route path ="/:warehouse/edit" element = {<Warehouse />} />} */}
         {<Route path ="/warehouse/add" element = {<AddWarehouseForm />} />}
-        {<Route path ="/warehouse/edit" element = {<EditWarehouseForm />} />}
+        {<Route path ="/warehouse/:warehouse_id/edit" element = {<EditWarehouseForm />} />}
         {<Route path ="/inventory" element = {<Inventory />} />}
-        <Route path ="/inventory/:itemId" element = {<InventoryDetails />} />
+        {<Route path ="/inventory/:itemId" element = {<InventoryDetails />} />}
         {<Route path ="/inventory/:itemId/edit" element = {<EditInventoryForm />} />}
         {/* {<Route path ="/inventory/add" element = {<AddInventory />} />} */}
         <Route path="/:id" element = {<WarehouseDetails />} />
