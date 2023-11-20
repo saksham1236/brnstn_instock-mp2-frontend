9f2315c34638ac947c69751cd366c5e7549c9679
saksham1236 <saksham1236@gmail.com>
2023-11-19 23:38:22 -0500

    Adding my changes, aggain??

--- src/components/AddInventory/AddInventory.jsx
+++ src/components/AddInventory/AddInventory.jsx
@@ -3,7 +3,7 @@ import InputComponent from "../Input/Input";
 import DropdownSelect from "../Dropdown/Dropdown";
 import RadioButtons from "../RadioButton/RadioButton";
 import ButtonEl from "../Button/Button";
-import { useRef } from "react";
+import { useState, useRef } from "react";
 import axios from "axios";
 import { useParams, useNavigate } from "react-router-dom";
 /**
@@ -15,39 +15,42 @@ import { useParams, useNavigate } from "react-router-dom";
 function AddInventory(props) {
 	const params = useParams();
 	const navigate = useNavigate();
-	let formData  = useRef({
+	const [showQuantity, setShowQuantity] = useState(true);
+	let formData = useRef({
 		warehouse_id: "",
 		item_name: "",
 		description: "",
 		category: "",
 		status: "",
-		quantity: ""
+		quantity: "",
 	});
 
-	function setFormData(newFormData){
+	function setFormData(newFormData) {
 		formData.current = newFormData;
 	}
 
 	const setWarehouseId = (newFormData) => {
 		const warehouse_name = newFormData.warehouse_name;
 		const warehouses = {
-			"Manhattan" : 1,
-			"Washington" : 2,
-			"Jersey" : 3,
-			"SF" : 4,
-			"Santa Monica" : 5,
-			"Seattle" : 6,
-			"Miami" : 7,
-			"Boston" : 8
-		}
+			Manhattan: 1,
+			Washington: 2,
+			Jersey: 3,
+			SF: 4,
+			"Santa Monica": 5,
+			Seattle: 6,
+			Miami: 7,
+			Boston: 8,
+		};
 
-		const id = warehouses[warehouse_name]
-		setFormData({...formData.current, ["warehouse_id"]: id});
-	}
-
+		const id = warehouses[warehouse_name];
+		setFormData({ ...formData.current, ["warehouse_id"]: id });
+	};
 
 	const onChangeFormhandler = (event) => {
-		setFormData({...formData.current, [event.target.name]: event.target.value});
+		setFormData({
+			...formData.current,
+			[event.target.name]: event.target.value,
+		});
 	};
 
 	const backButtonHandler = (event) => {
@@ -57,28 +60,46 @@ function AddInventory(props) {
 
 	const postData = async (postFormData) => {
 		axios
-		.post(`http://localhost:8080/inventories/`, postFormData)
-		.then((res) => {
-			alert(`Item has been added successfully ${res.status}`)
-		})
-		.catch((err) =>{
-			alert(`Axios error adding item details,  http://localhost:8080/inventories/: ${err}`)
-		})
-	}
+			.post(`http://localhost:8080/inventories/`, postFormData)
+			.then((res) => {
+				alert(`Item has been added successfully ${res.status}`);
+			})
+			.catch((err) => {
+				alert(
+					`Axios error adding item details,  http://localhost:8080/inventories/: ${err}`
+				);
+			});
+	};
 
 	const formHandler = (event) => {
 		event.preventDefault();
 		setWarehouseId(formData.current);
 		console.log(formData.current);
 		//Form validation logic
-		if(formData.warehouse_id | formData.item_name |  formData.category | formData.category === "Please select a category" | formData.quantity | formData.warehouse_name | formData.warehouse_name === "Please select a Warehouse") {
+		if (
+			formData.warehouse_id |
+			formData.item_name |
+			formData.category |
+			(formData.category === "Please select a category") |
+			formData.quantity |
+			formData.warehouse_name |
+			(formData.warehouse_name === "Please select a Warehouse")
+		) {
 			alert("Invalid data");
-			return
+			return;
 		}
-	
+
 		postData(formData.current);
 	};
-	
+
+	function showQuantityFunc(show) {
+		if (show === "In Stock") {
+			setShowQuantity(true);
+		} else {
+			setShowQuantity(false);
+		}
+	}
+
 	return (
 		<>
 			<TitleComponent
@@ -92,8 +113,7 @@ function AddInventory(props) {
 						<form
 							className='inventory-edit__form'
 							onSubmit={formHandler}
-							onChange={onChangeFormhandler}
-						>
+							onChange={onChangeFormhandler}>
 							<div className='inventory-edit__form__row'>
 								<div className='inventory-edit__form__column'>
 									<h2 className='inventory-edit__form__header'>
@@ -103,14 +123,14 @@ function AddInventory(props) {
 										labelName='Item Name'
 										fieldName='item_name'
 										defaultValue='Add Item Name'
-										error = {false}
+										error={false}
 									/>
 									<InputComponent
 										labelName='Description'
 										type='textarea'
 										defaultValue='Add a description'
 										fieldName='description'
-										error-description = {false}
+										error-description={false}
 									/>
 									<DropdownSelect
 										labelName='Category'
@@ -124,7 +144,7 @@ function AddInventory(props) {
 										]}
 										defaultValue='Please select a category'
 										fieldName='category'
-										error-category = {false}
+										error-category={false}
 									/>
 								</div>
 
@@ -135,16 +155,23 @@ function AddInventory(props) {
 									<RadioButtons
 										labelName='Status'
 										items={["In Stock", "Out of Stock"]}
-										error-status = {false}
+										error-status={false}
 										fieldName='status'
-										defaultValue = {`${formData.status | "In Stock"}`}
+										defaultValue={`${
+											formData.current.status
+										}`}
+										onChange = {(event) => showQuantityFunc(event.target.value)}
 									/>
-									<InputComponent
-										labelName='Quantity'
-										defaultValue='0'
-										fieldName='quantity'
-										error-quantity = {false}
-									/>
+
+									{showQuantity && (
+										<InputComponent
+											labelName='Quantity'
+											defaultValue='0'
+											fieldName='quantity'
+											error-quantity={false}
+										/>
+									)}
+
 									<DropdownSelect
 										labelName='Warehouse'
 										items={[
@@ -159,7 +186,7 @@ function AddInventory(props) {
 											"Boston",
 										]}
 										defaultValue='Please select a Warehouse'
-										error = {false}
+										error={false}
 										fieldName='warehouse_name'
 									/>
 								</div>
